98508c5697ad3257c65fd74482e26950181295ad
varghese <varghese.t.mascreen20@gmail.com>
2022-05-10 21:50:03 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/gsign_in/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gsign_in
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'home.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
